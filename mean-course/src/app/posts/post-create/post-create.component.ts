d85fe7fabccb53d8dfa647758e6c3991e9ddd31f
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-07 20:29:12 +0200

    finishing the angular front end

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { PostsService } from './../posts.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+// import { Post } from '../post.model';
 
 @Component({
   selector: 'post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
 
-  newPost = 'NO CONTENT'
+export class PostCreateComponent implements OnInit {
 
-  onAddPost(postInput: HTMLTextAreaElement){
-    console.dir(postInput)
-    this.newPost = postInput.value
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor( private postService : PostsService){}
+
+  ngOnInit(): void {
+
+  }
+
+  onAddPost(form: NgForm) {
+    if(form.invalid) return
+    this.postService.addPosts(form.value.title,form.value.content);
+    form.resetForm();
   }
 
 }
---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,34 @@
+import { PostsService } from './../posts.service';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+
+export class PostListComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = []
+  private postsSubscriber : Subscription;
+
+  constructor(private postService: PostsService) {
+
+  }
+  ngOnDestroy(): void {
+    this.postsSubscriber.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+    this.postsSubscriber = this.postService.getPostUpdateListener()
+    .subscribe((posts: Post[])=>{
+      this.posts = posts;
+    })
+  }
+
+}
