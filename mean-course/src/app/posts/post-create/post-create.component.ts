5ed65b7a9cce0787166b1657514a0c7bfc6d0ef6
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-11 14:18:20 +0200

    adding Image Preview

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { takeWhile } from 'rxjs';
 
 // import { Post } from '../post.model';
 
@@ -21,6 +22,7 @@ export class PostCreateComponent implements OnInit {
   isLoading = false;
   post: Post;
   form: FormGroup;
+  imagePreview: string;
 
   constructor(private postService: PostsService, public route: ActivatedRoute) { }
 
@@ -33,6 +35,10 @@ export class PostCreateComponent implements OnInit {
       content: new FormControl(
         null,
         { validators: [Validators.required] }
+      ),
+      image: new FormControl(
+        null,
+        { validators: [Validators.required]}
       )
     });
 
@@ -62,6 +68,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     })
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file})
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = ()=>{
+      this.imagePreview = reader.result as string
+    }
+    reader.readAsDataURL(file)
   }
 
   onSavePost() {
