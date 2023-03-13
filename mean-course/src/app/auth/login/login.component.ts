833f2e4e2a292d18f946d917b8ffad747c9e9bf8
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-13 10:20:09 +0200

    handling user Input Login

--- mean-course/src/app/auth/login/login.component.ts
+++ mean-course/src/app/auth/login/login.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +11,10 @@ export class LoginComponent implements OnInit {
   isLoading: false;
 
   constructor() { }
+
+  onLogin(form: NgForm){
+    console.log(form.value)
+  }
 
   ngOnInit(): void {
   }
