cd8d5e56922e4704a26363004dea0c1f26bd3a0d
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-13 13:53:03 +0200

    implementing SPA authentication

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const router = express.Router();
 
-const User = require("../models/user")
+const User = require("../models/user");
+const user = require('../models/user');
 
 router.post("/signup", (req,res,next)=>{
   bcrypt.hash(req.body.password,10).then(
@@ -30,5 +32,23 @@ router.post("/signup", (req,res,next)=>{
   )
 
 })
+
+router.post("/login", (req,res,next)=>{
+  User.findOne({email : req.body.email}).then(
+    user=>{
+      if(!user){return res.status(404).send("Auth Failed")}
+      return  bcrypt.compare(req.body.password, user.password)
+    }
+  ).then(
+    result=>{
+      if(!result) return res.status(404).send("Auth Failed")
+      const token = jwt.sign({email: user.email, userId: user._id}, 'secret_this_should_be_longer',
+      {expiresIn: '1h'})
+    }
+  ).catch(
+    err =>{ return res.status(404).send("Auth Failed")}
+  )
+})
+
 
 module.exports = router
