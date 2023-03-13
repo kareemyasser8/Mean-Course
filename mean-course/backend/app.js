e0d66313a63975dc9531790d446991aca6cbe6f4
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-13 11:09:57 +0200

    creating the userModel

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts')
+const userRoutes = require('./routes/user')
 
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type, Accept');
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
 })
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 
 module.exports = app;
