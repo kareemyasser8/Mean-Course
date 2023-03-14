b9550912189fc81a76eb881ec5a1c1990df3241f
kareemyasser8 <kareem.yasserr@gmail.com>
2023-03-14 11:09:48 +0200

    improve the UI header to reflect the auth status

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { AuthData } from './auth-data.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private token: string
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient){
 
@@ -14,6 +16,10 @@ export class AuthService {
 
   getToken(){
     return this.token;
+  }
+
+  getAuthStatusListener(){
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email:string,password: string){
@@ -30,6 +36,7 @@ export class AuthService {
     .subscribe(response=>{
       const token = response.token
       this.token = token;
+      this.authStatusListener.next(true);
     })
   }
 
--- mean-course/src/app/posts/header/header.component.ts
+++ mean-course/src/app/posts/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../../auth/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListnenerSubs: Subscription
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authListnenerSubs = this.authService.getAuthStatusListener()
+    .subscribe(
+      isAuthenticated =>{
+        this.userIsAuthenticated = isAuthenticated;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authListnenerSubs.unsubscribe();
   }
 
 }
